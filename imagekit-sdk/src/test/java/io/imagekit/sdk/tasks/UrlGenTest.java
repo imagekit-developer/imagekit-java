01450c5e15ab6cf7eb01ce60d7794f98fadf85d2
Manu Chaudhary <manuchaudhary@Manus-MacBook-Pro.local>
2020-03-16 12:22:41 +0530

    updated test case and readme

--- imagekit-sdk/src/test/java/io/imagekit/sdk/tasks/UrlGenTest.java
+++ imagekit-sdk/src/test/java/io/imagekit/sdk/tasks/UrlGenTest.java
@@ -21,4 +21,12 @@ public class UrlGenTest {
         assertThat("41b3075c40bc84147eb71b8b49ae7fbf349d0f00",is(signature));
 
     }
+
+    @Test
+    public void signUrl_tests_without_slash_expire_zero() {
+        String url="https://test-domain.com/test-endpoint/tr:w-100/test-signed-url.png";
+        String signature = UrlGen.signUrl("private_key_test", url, "https://test-domain.com/test-endpoint", 0);
+        assertThat("41b3075c40bc84147eb71b8b49ae7fbf349d0f00",is(signature));
+
+    }
 }
